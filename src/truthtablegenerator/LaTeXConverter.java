0083b3fed78543197ab1673022c7cd4fed0b6442
BryceLawrence <bryce.lawrence@gmail.com>
2014-11-15 20:20:13 -0700

    LaTeXConverter written

--- src/truthtablegenerator/LaTeXConverter.java
+++ src/truthtablegenerator/LaTeXConverter.java
@@ -6,9 +6,29 @@
 package truthtablegenerator;
 
 /**
- *
+ * Used for converting a string to an TeX version of that string.
  * @author Bryce
  */
 public class LaTeXConverter {
+    /**
+     * Static Method to convert a string to a TeX string.
+     * @param toConvert The string to be converted to TeX
+     * @return 
+     */
+    public static String toTex(String toConvert) {
+        String converted = null;
+        if(!toConvert.equals(null)) {
+            converted = toConvert;
+            converted = converted.replaceAll("\\*", "\\land ");
+            converted = converted.replaceAll("\\+", "\\lor ");
+            converted = converted.replaceAll("~", "\\lnot ");
+            converted = converted.replaceAll(">", "\\RightArrow ");
+            converted = converted.replaceAll("<", "\\LeftRightarrow ");
+            converted = converted.replaceAll("#", "\\oplus ");            
+        }
+            
+        return converted;
+    }
     
 }
+
