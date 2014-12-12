0263df877ad1d1e21b572ce75e6e00e5c394b140
McAllister <scr08001@byui.edu>
2014-12-12 12:11:58 -0700

    also testng needs to be pushed

--- test/TruthTableGeneratorTest.java
+++ test/TruthTableGeneratorTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -14,8 +9,9 @@ import truthtablegenerator.Expression;
 import truthtablegenerator.ValidationException;
 
 /**
- *
- * @author Bryce
+ * A test class for our project
+ * 
+ * @author Bryce, McAllister, Tyler
  */
 public class TruthTableGeneratorTest {
 
